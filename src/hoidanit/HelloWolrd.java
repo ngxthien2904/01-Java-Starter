4f5ad607c6fc318de31ba5e9831625a234141472
Xuan Thien <nguyenxuanthien29042001@gmail.com>
2025-07-01 12:54:39 +0700

    init project

---
+++ src/hoidanit/HelloWolrd.java
@@ -0,0 +1,10 @@
+package hoidanit;
+
+public class HelloWolrd {
+	public static void main(String[] args) {
+		System.out.println("Hello World with java anh Hỏi dân IT");
+		System.out.println("Hello World with java anh Hỏi dân IT");
+		System.out.println("Hello World with java anh Hỏi dân IT");
+		System.out.println("Hello World with java anh Hỏi dân IT");
+	}
+}
