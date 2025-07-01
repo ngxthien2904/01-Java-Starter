a2b2a747c23b176cb033cc35e12a639572e1024d
Xuan Thien <nguyenxuanthien29042001@gmail.com>
2025-07-01 13:12:43 +0700

    Variables

--- src/hoidanit/HelloWolrd.java
+++ src/hoidanit/HelloWolrd.java
@@ -2,9 +2,11 @@ package hoidanit;
 
 public class HelloWolrd {
 	public static void main(String[] args) {
-		System.out.println("Hello World with java anh Hỏi dân IT");
-		System.out.println("Hello World with java anh Hỏi dân IT");
-		System.out.println("Hello World with java anh Hỏi dân IT");
-		System.out.println("Hello World with java anh Hỏi dân IT");
+		int a = 6;
+		int b = 9;
+		int c = a + b;
+		int test = 5 + 9 * 3 - 10 / 2;
+		System.out.println("Sum: " + c);
+		System.err.println("Check test: " + test);
 	}
 }
