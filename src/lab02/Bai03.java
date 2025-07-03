24773ec62cb202765824d07cc4d4aeede8018fe6
Xuan Thien <nguyenxuanthien29042001@gmail.com>
2025-07-03 15:38:33 +0700

    Lab02 - Bai03

--- src/lab02/Bai03.java
+++ src/lab02/Bai03.java
@@ -1,8 +1,23 @@
 package lab02;
 
+import java.util.Scanner;
+
 public class Bai03 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.print("Nhập số điện: ");
+		int a = scanner.nextInt();
+
+		if (0 <= a && a <= 100) {
+			int x = a * 1000;
+			System.out.println("Số tiền điện: " + x);
+		} else {
+			int x = a * 1000 + (a - 100) * 1500;
+			System.out.println("Số tiền điện: " + x);
+		}
+
+		scanner.close();
 	}
 
 }
