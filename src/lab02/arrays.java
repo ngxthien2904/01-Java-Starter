25dd334f9de1601cb1c891f1786dcb3a6e584803
Xuan Thien <nguyenxuanthien29042001@gmail.com>
2025-07-04 03:09:36 +0700

    Arrays - method

--- src/lab02/arrays.java
+++ src/lab02/arrays.java
@@ -4,26 +4,24 @@ import java.util.Arrays;
 
 public class arrays {
 	public static void main(String[] args) {
-		String[] clubs = { "Mu", "Man", "Liver" };
-		clubs[0] = "Ars"; // thay đổi giá trị trong mảng
+		int[] clubs = { 29, 0, 4, 2, 1, 9 };
 
-		String[] a = new String[5];
-		a[0] = "1";
+		System.out.println(Arrays.toString(clubs)); // hiển thị mảng
 
-		System.out.println("gia tri a[1] la: " + a[1]);
-
-		System.out.println("run here array" + Arrays.toString(clubs)); // hiển thị mảng
-
-		System.out.println("run here array with length " + clubs.length); // chieu dai cua mang
-
-		System.out.println("run here array first element: " + clubs[0]);
-
-		System.out.println("run here array second element: " + clubs[1]);
-
-		String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-		for (int i = 0; i < cars.length; i++) { // duyệt tất cả phần tử của mảng
-			System.out.println(cars[i]);
+		// Sắp xếp thủ công
+		for (int i = 0; i < clubs.length - 1; i++) {
+			for (int j = i + 1; j < clubs.length; j++) {
+				if (clubs[i] < clubs[j]) {
+					int temp = clubs[i];
+					clubs[i] = clubs[j];
+					clubs[j] = temp;
+				}
+			}
 		}
+		System.out.println(Arrays.toString(clubs));
+
+//		Arrays.sort(clubs); // method sắp xếp
+//		System.out.println("run here array" + Arrays.toString(clubs));
 	}
 
 }
