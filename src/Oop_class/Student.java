8862a4c5d178ce0748aa2747d31baf1c1607ccb9
Xuan Thien <nguyenxuanthien29042001@gmail.com>
2025-07-11 06:04:10 +0700

    class-object-method

---
+++ src/Oop_class/Student.java
@@ -0,0 +1,20 @@
+package Oop_class;
+
+public class Student {
+
+	// class attributes
+	String name;
+	int age;
+	String fullName;
+	String firstName;
+	String emailAddress;
+
+	// class method (class function)
+	public void learnJava() {
+		System.out.println("??");
+	}
+
+	int getAge() {
+		return 25;
+	}
+}
---
+++ src/Oop_class/objectStudent.java
@@ -0,0 +1,19 @@
+package Oop_class;
+
+public class objectStudent {
+	int sum(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		// Khai báo object và gán giá trị
+//		Student st1 = new Student();
+//		st1.name = "Thien";
+//		st1.age = 24;
+//		System.out.println("Student: " + st1.name + "\n" + "age: " + st1.age);
+
+		objectStudent test = new objectStudent();
+		int a = test.sum(6, 9);
+		System.out.println("a = " + a);
+	}
+}
